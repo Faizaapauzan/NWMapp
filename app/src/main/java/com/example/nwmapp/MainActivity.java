c1909535ce4565a8beb9b71c3bf161cb95808d68
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-07-05 09:50:21 +0800

    Faiza 5/7/2022

--- app/src/main/java/com/example/nwmapp/MainActivity.java
+++ app/src/main/java/com/example/nwmapp/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.nwmapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        EditText Username =(EditText) findViewById(R.id.Username);
+        EditText Password =(EditText) findViewById(R.id.Password);
+
+        MaterialButton LoginButton = (MaterialButton) findViewById(R.id.LoginButton);
+
+        //admin and admin
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Username.getText().toString().equals("admin") && Password.getText().toString().equals("admin123")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+
+                    openNewActivity();
+                    
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void openNewActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
