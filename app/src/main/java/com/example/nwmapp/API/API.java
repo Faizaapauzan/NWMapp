d53403ab4813837ec2f4a6b192beef97a061f35a
iThinksusan <iThinksusan.mfgsoln@gmail.com>
2022-08-03 16:42:01 +0800

    Susan 03/08/2022

--- app/src/main/java/com/example/nwmapp/API/API.java
+++ app/src/main/java/com/example/nwmapp/API/API.java
@@ -1,10 +1,14 @@
 package com.example.nwmapp.API;
 
 import com.example.nwmapp.models.DefaultResponse;
+import com.example.nwmapp.models.JobAssign;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface API {
@@ -16,12 +20,8 @@ public interface API {
             @Field("password") String password
     );
 
-    @POST("job")
-    @FormUrlEncoded
-    Call<DefaultResponse> getJobAssign (
-            @Field("username") String username,
-            @Field("password") String password
-    );
+    @GET("job")
+    Call<List<JobAssign>>getJobAssign();
 
 
 }
